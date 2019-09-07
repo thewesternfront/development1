126542dc5e03eafb8c97a65330f8c1ad8799b634
thewesternfront <cymruclan@icloud.com>
2019-09-07 14:40:47 -0700

    added a class and a method

---
+++ maintest/firstprog/MyClass.py
@@ -0,0 +1,8 @@
+
+
+
+class MyClass:
+
+    def printstuff(self):
+        print("stuff")
+
--- maintest/firstprog/main.py
+++ maintest/firstprog/main.py
@@ -1,1 +1,12 @@
-print("This is a test")
+
+import MyClass as mc
+
+def main():
+    print("This is a test")
+    print("going to the classes method")
+    mc.MyClass.printstuff(mc.MyClass)
+
+
+
+if __name__ == "__main__":
+    main()
